4629b9c44fa84950654693a82b18bc05a2be7ff3
Tuong Phan <tramphongsts@gmail.com>
2021-04-14 20:34:51 +0700

    numberToSpeech: speak integer successfully

--- dist/functions.js
+++ dist/functions.js
@@ -21844,7 +21844,7 @@ var __generator = this && this.__generator || function (thisArg, body) {
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.QRCode = exports.StringFontFormatter = exports.StringCellFormatter = exports.KipThi = exports.TyGia = exports.TacGia = void 0;
+exports.numberToSpeech = exports.QRCode = exports.StringFontFormatter = exports.StringCellFormatter = exports.KipThi = exports.TyGia = exports.TacGia = void 0;
 /**
  * Returns author's name and ID in 4 cells.
  * @customfunction
@@ -22244,12 +22244,221 @@ function QRCode(text, ShapeName, invocation) {
 }
 
 exports.QRCode = QRCode;
+/**
+ * Speak the input number.
+ * @customfunction
+ * @param number Number to be spoken.
+ */
+
+function numberToSpeech(number) {
+  console.log(typeof number);
+  console.log(number);
+  var chuSo = [" không", " một", " hai", " ba", " bốn", " năm", " sáu", " bẩy", " tám", " chín"];
+  var audioChuSo = [new Audio("../../sound/0.wav"), new Audio("../../sound/1.wav"), new Audio("../../sound/2.wav"), new Audio("../../sound/3.wav"), new Audio("../../sound/4.wav"), new Audio("../../sound/5.wav"), new Audio("../../sound/6.wav"), new Audio("../../sound/7.wav"), new Audio("../../sound/8.wav"), new Audio("../../sound/9.wav")];
+  var tien = [" GH", " nghìn", " triệu", " tỷ", " nghìn tỷ", " triệu tỷ"];
+  var audioTien = [new Audio("../../sound/GolfHit.wav"), new Audio("../../sound/nghin.wav"), new Audio("../../sound/trieu.wav"), new Audio("../../sound/ty.wav"), new Audio("../../sound/nghinTy.wav"), new Audio("../../sound/trieuTy.wav")];
+  var biggestNumber = 8999999999999999;
+  var lan,
+      i,
+      so,
+      ketqua = "",
+      tmp = "";
+  var vitri = new Array(6);
+  var sounds = new Array();
+
+  if (typeof number != "number") {
+    console.log("check number: not number");
+  } else {
+    console.log("check number: number");
+
+    if (number == 0) {
+      console.log("number == 0");
+      sounds.push(audioChuSo[0]);
+      sounds.push(audioTien[0]);
+      ketqua += chuSo[0];
+    }
+
+    if (number > 0) {
+      console.log("number > 0");
+      so = number;
+    } else {
+      console.log("number <0");
+      so = -number;
+    }
+
+    if (number > biggestNumber) {
+      return "";
+    }
+
+    vitri[5] = Math.floor(so / 1000000000000000);
+    console.log("vitri[5] = " + vitri[5]);
+    so -= vitri[5] * 1000000000000000;
+    vitri[4] = Math.floor(so / 1000000000000);
+    console.log("vitri[4] = " + vitri[4]);
+    so -= vitri[4] * 1000000000000;
+    vitri[3] = Math.floor(so / 1000000000);
+    console.log("vitri[3] = " + vitri[3]);
+    so -= vitri[3] * 1000000000;
+    vitri[2] = Math.floor(so / 1000000);
+    console.log("vitri[2] = " + vitri[2]);
+    vitri[1] = Math.floor(so % 1000000 / 1000);
+    console.log("vitri[1] = " + vitri[1]);
+    vitri[0] = so % 1000;
+    console.log("vitri[0] = " + vitri[0]);
+
+    if (vitri[5] > 0) {
+      lan = 5;
+    } else if (vitri[4] > 0) {
+      lan = 4;
+    } else if (vitri[3] > 0) {
+      lan = 3;
+    } else if (vitri[2] > 0) {
+      lan = 2;
+    } else if (vitri[1] > 0) {
+      lan = 1;
+    } else {
+      lan = 0;
+    }
+
+    console.log("lan = " + lan);
+
+    for (i = lan; i >= 0; i--) {
+      console.log("i in for = " + i);
+      tmp = docSo3ChuSo(vitri[i]);
+      console.log("tmp = " + tmp);
+      ketqua += tmp;
+      console.log("ketqua inside for loop lan= " + ketqua);
+
+      if (vitri[i] != 0 || i == 0) {
+        if (i == 0 && ketqua.substring(ketqua.length - 1) == ",") ketqua = ketqua.substring(0, ketqua.length - 1);
+        ketqua += tien[i];
+        sounds.push(audioTien[i]);
+      }
+
+      console.log("ketqua before adding , = " + ketqua);
+      if (i > 0 && tmp != "") ketqua += ",";
+      console.log("ketqua = after adding , = " + ketqua);
+    } //console.log("ketqua.substring(ketqua.length - 1) = " + ketqua.substring(ketqua.length - 1));
+
+
+    if (ketqua.substring(ketqua.length - 1) == ",") ketqua = ketqua.substring(0, ketqua.length - 1);
+    ketqua = ketqua.trim();
+
+    if (number < 0) {
+      ketqua = "âm " + ketqua;
+      sounds.unshift(new Audio("../../sound/am.wav"));
+    }
+
+    console.log("ketqua after add - = " + ketqua); // var audio = new Audio("../../sound/0.wav");
+    // var audio2 = new Audio("../../sound/1.wav");
+    // sounds.push(audio);
+    // sounds.push(audio2);
+    // sounds.push(audio);
+
+    var soundIndex = -1; //console.log("sounds.length outside func = " + sounds.length);
+
+    playSnd();
+    return ketqua.substring(0, 1).toUpperCase() + ketqua.substring(1);
+  }
+
+  function playSnd() {
+    //console.log("sounds.length inside func = " + sounds.length);
+    soundIndex++;
+
+    if (soundIndex == sounds.length) {
+      return;
+    }
+
+    sounds[soundIndex].addEventListener("ended", playSnd);
+    sounds[soundIndex].play();
+  }
+
+  function docSo3ChuSo(baso) {
+    console.log("baso = " + baso);
+    var tram, chuc, donvi;
+    var ketQua = "";
+    tram = Math.floor(baso / 100);
+    console.log("tram = " + tram);
+    chuc = Math.floor(baso % 100 / 10);
+    console.log("chuc = " + chuc);
+    donvi = baso % 10;
+    console.log("donvi = " + donvi);
+
+    if (tram == 0 && chuc == 0 && donvi == 0) {
+      return "";
+    }
+
+    if (ketqua.substring(ketqua.length - 1) == "," || tram != 0) {
+      ketQua += chuSo[tram] + " trăm";
+      sounds.push(audioChuSo[tram]);
+      sounds.push(new Audio("../../sound/tram.wav"));
+
+      if (chuc == 0 && donvi != 0) {
+        ketQua += " linh";
+        sounds.push(new Audio("../../sound/linh.wav"));
+      }
+    }
+
+    if (chuc != 0 && chuc != 1) {
+      ketQua += chuSo[chuc] + " mươi";
+      sounds.push(audioChuSo[chuc]);
+      sounds.push(new Audio("../../sound/muoi.wav"));
+
+      if (chuc == 0 && donvi != 0) {
+        ketQua += " linh";
+        sounds.push(new Audio("../../sound/linh.wav"));
+      }
+    }
+
+    if (chuc == 1) {
+      ketQua += " mười";
+      sounds.push(new Audio("../../sound/10.wav"));
+    }
+
+    switch (donvi) {
+      case 1:
+        if (chuc != 0 && chuc != 1) {
+          ketQua += " mốt";
+          sounds.push(new Audio("../../sound/mot.wav"));
+        } else {
+          ketQua += chuSo[donvi];
+          sounds.push(audioChuSo[donvi]);
+        }
+
+        break;
+
+      case 5:
+        if (chuc == 0) {
+          ketQua += chuSo[donvi];
+          sounds.push(audioChuSo[donvi]);
+        } else {
+          ketQua += " lăm";
+          sounds.push(new Audio("../../sound/lam.wav"));
+        }
+
+        break;
+
+      default:
+        if (donvi != 0) {
+          ketQua += chuSo[donvi];
+          sounds.push(audioChuSo[donvi]);
+        }
+
+        break;
+    }
+
+    return ketQua;
+  }
+}
+
+exports.numberToSpeech = numberToSpeech;
 CustomFunctions.associate("TACGIA", TacGia);
 CustomFunctions.associate("TYGIA", TyGia);
 CustomFunctions.associate("KIPTHI", KipThi);
 CustomFunctions.associate("STRINGCELLFORMATTER", StringCellFormatter);
 CustomFunctions.associate("STRINGFONTFORMATTER", StringFontFormatter);
 CustomFunctions.associate("QRCODE", QRCode);
+CustomFunctions.associate("NUMBERTOSPEECH", numberToSpeech);
 
 /***/ })
 
